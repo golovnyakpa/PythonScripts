63d5c9deaa785f482d5bd859a9536f1f33a32047
Pavel Golovnyak <pgolovnyak@gmail.com>
2019-10-04 12:14:18 +0300

    Update lrucache.py

--- lrucache.py
+++ lrucache.py
@@ -16,7 +16,7 @@ class LRUCacheDecorator:
         if (isinstance(ttl, float)) or (isinstance(ttl, int)):
             self.ttl = ttl
         else:
-            self.ttl = 20
+            self.ttl = 10000000
         self.maxsize = maxsize
 
     def __call__(self, function):
@@ -25,23 +25,27 @@ class LRUCacheDecorator:
         cache = {}
 
         def wrapped(*args, **kwargs):
+            current_milli_time = lambda: time.time() * 1000
             args_hash = hash(tuple([args, frozenset(kwargs)]))
             if args_hash not in cache:
                 if len(cache) == self.maxsize:
                     self.delete_lru_record(cache)
                 cache[args_hash] = [function(*args, **kwargs),
-                                    time.time(), time.time()]
+                                    current_milli_time(), 
+                                    current_milli_time()]
             else:
-                if time.time() - cache[args_hash][1] > self.ttl:
+                if current_milli_time() - cache[args_hash][1] > self.ttl:
                     cache[args_hash] = [function(*args, **kwargs),
-                                        time.time(), time.time()]
+                                        current_milli_time(), 
+                                        current_milli_time()]
                 else:
-                    cache[args_hash][2] = time.time()
+                    cache[args_hash][2] = current_milli_time()
             return cache[args_hash][0]
         return wrapped
 
     def delete_lru_record(self, cache):
-        min_time = time.time()
+        min_time = time.time() * 1000
+        min_key = 0
         for key, value in cache.items():
             if value[2] < min_time:
                 min_time = value[2]
